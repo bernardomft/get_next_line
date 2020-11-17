53e4d954d278f86224baf9b3744bffa52469b89f
Bernardo Moran fernandez-trigales <bmoran-f@c2r13s3.42madrid.com>
2020-11-17 12:58:10 +0100

    Norma de getx_next_line-.h finiquitada

--- get_next_line.h
+++ get_next_line.h
@@ -3,33 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bmoran-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: bmoran-f <bmoran-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/18 16:41:07 by bmoran-f          #+#    #+#             */
-/*   Updated: 2020/01/18 16:41:11 by bmoran-f         ###   ########.fr       */
+/*   Created: 2020/11/17 12:45:27 by bmoran-f          #+#    #+#             */
+/*   Updated: 2020/11/17 12:56:45 by bmoran-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE 
-#define BUFFER_SIZE 32
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
 
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
 # include <stdio.h>
 
-int         get_next_line(int fd,char **line);
-char	    *ft_strdup( char *s1);
-char	    *ft_strjoin(char  *s1, char  *s2);
-char	    *ft_substr(char  *s, unsigned int start, size_t len);
-size_t		ft_strlen( char *str);
-char	    *ft_strchr( char *s, int c);
-size_t	    ft_stchr_pos(char *s, char c);
-void	    *ft_memset(void *str, int c, size_t n);
+int			get_next_line(int fd, char **line);
+char		*ft_strdup(char *s1);
+char		*ft_strjoin(char *s1, char *s2);
+char		*ft_substr(char *s, unsigned int start, size_t len);
+size_t		ft_strlen(char *str);
+char		*ft_strchr(char *s, int c);
+size_t		ft_stchr_pos(char *s, char c);
+void		*ft_memset(void *str, int c, size_t n);
 
 #endif
-
